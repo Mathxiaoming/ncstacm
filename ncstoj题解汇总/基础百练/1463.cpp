ab78dc0e19359e9011caa302fc0fc2f8b6f7bdc4
zhbink <zhbink@163.com>
2019-04-03 20:49:39 +0800

    Create 1463.cpp

---
+++ ncstoj题解汇总/基础百练/1463.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+bool check(string word) {
+    int cnt = 0;
+    for (int i = 0; i < word.length(); i ++ ) if(word[i] <= 'Z') cnt++;
+    return ((cnt==0 || cnt==word.length()) || (cnt==1 && word[0]<='Z'));
+}
+int main() {
+    string word;
+    cin >> word;
+    check(word) ? puts("True") : puts("False");
+    return 0;
+}
