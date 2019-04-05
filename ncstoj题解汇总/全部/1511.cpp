341a67fe4392d003dc7afe2918ea0e73c14ecf9b
zhbink <zhbink@163.com>
2019-04-05 09:53:51 +0800

    Update 1511.cpp

--- ncstoj题解汇总/全部/1511.cpp
+++ ncstoj题解汇总/全部/1511.cpp
@@ -2,23 +2,18 @@
 using namespace std;
 
 const int maxn = 100010;
-int prime[maxn]; // 存放所有素数
-int tot;         // 素数的个数
-bool p[maxn];    // p[i]=true代表p[i]是素数
+int prime[maxn];  // 存放所有素数
+int tot;  // 素数的个数
+bool v[maxn];
 
 // maxn以内的素数放进prime[]中
 void findPrime(int n)
 {
-    for (int i = 1; i <= n; i++)
-        p[i] = 1;
     for (int i = 2; i <= n; i++)
     {
-        if (p[i])
-        {
-            prime[tot++] = i;
-            for (int j = i; j <= n / i; j++)
-                p[i * j] = false;
-        }
+        if (v[i]) continue;
+        prime[tot++] = i;
+        for (int j = i; j <= n/i; j ++) v[i*j] = 1;
     }
 }
 
