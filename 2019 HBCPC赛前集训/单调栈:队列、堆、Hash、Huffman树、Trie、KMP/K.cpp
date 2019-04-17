b0c29d056f38fbf66982817b9e804a0f52ae8822
zhbink <zhbink@163.com>
2019-04-17 16:13:22 +0800

    Create K.cpp

---
+++ 2019 HBCPC赛前集训/单调栈:队列、堆、Hash、Huffman树、Trie、KMP/K.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <queue>
+#include <stdio.h>
+using ll = long long;
+using namespace std;
+
+priority_queue<ll> pq;
+int n;
+
+int main()
+{
+    ll num, sum = 0;
+    cin >> n;
+    for (int i = 0; i < n; i ++)
+    {
+        scanf("%lld", &num);
+        pq.push(-num);
+    }
+
+    while (pq.size() != 1)
+    {
+        num = 0;
+        for (int i = 0; i <= 1; i++)
+        {
+            num += pq.top();
+            pq.pop();
+        }
+        sum += num;
+        pq.push(num);
+    }
+    cout << -sum << endl;
+    return 0;
+}
