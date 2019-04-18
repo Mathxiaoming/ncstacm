48c9fd89776037c7bb4e2bd99ed6a90a2101fc8a
zhbink <zhbink@163.com>
2019-04-18 10:14:19 +0800

    Update L.cpp

--- 2019 HBCPC赛前集训/单调栈:队列、堆、Hash、Huffman树、Trie、KMP/L.cpp
+++ 2019 HBCPC赛前集训/单调栈:队列、堆、Hash、Huffman树、Trie、KMP/L.cpp
@@ -6,6 +6,19 @@ using ll = long long;
 const int p = 1e5+3;  // p可以是任意比较大的素数，太小(如131)会相当于普通数组导致超时
 vector<vector<int> > head[p+5];
 
+// 求雪花a的哈希值
+int H(vector<int> a)
+{
+    //对于6个数都相同的雪花，他们6个数之和、6个数之积都应该相等
+    int sum = 0, mul = 1;
+    for (int i = 0; i < 6; i ++)
+    {
+        sum = (sum + a[i]) % p;
+        mul = (ll)mul * a[i] % p;
+    }
+    return (sum + mul) % p;
+}
+
 // 判断两个向量正逆序之一是否相同
 // 1 2 3 4 5 6
 // 4 3 2 1 6 5
@@ -47,25 +60,26 @@ int main()
     for (int i = 1; i <= n; i++)
     {
         vector<int> a(6);
-        int sum = 0;
+        int val = 0;
         for (int j = 0; j < 6; j++)
         {
             scanf("%d", &a[j]);
             // 哈希：取模，注意乘法要避免溢出
-            sum = (sum + (ll)a[j] * a[j] % p) % p;
+            // val = (val + (ll)a[j] * a[j] % p) % p;  // 可以代替下面一行的 通过函数求哈希值
         }
+        val = H(a);
 
         // 邻接表存储每个雪花的信息，可以把查找的时间降到O(1)
-        // 对于每一个在相同表头head[sum]的元素，他们6个数的和取模后都为sum
+        // 对于每一个在相同表头head[val]的元素，他们6个数都相同，只需要看6个数是否满足正逆序之一相同的条件
 
-        // 遍历表头head[sum]指向的链表，寻找形状相同的雪花
-        for (int j = 0; j < head[sum].size(); j++)
-            if (isEqual(a, head[sum][j]))
+        // 遍历表头head[val]指向的链表，寻找形状相同的雪花
+        for (int j = 0; j < head[val].size(); j++)
+            if (isEqual(a, head[val][j]))
             {
                 puts("Twin snowflakes found.");
                 return 0;
             }
-        head[sum].push_back(a);
+        head[val].push_back(a);
     }
     puts("No two snowflakes are alike.");
     return 0;
